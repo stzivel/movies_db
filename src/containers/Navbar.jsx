76902c56f77c3bf358bba6bd0add98d69536fe0c
spyros <stzivel@gmail.com>
2023-03-13 21:30:35 +0200

    .

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,21 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {ImSearch} from 'react-icons/im'
+import { Link } from 'react-router-dom';
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
 
 function Navbar() {
-  return (
-    <div className='navbar w-full h-full'>
 
-        <div className="navbar__container flex justify-center text-center py-5 ">
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleToggle=()=>{
+    setToggleMenu(!toggleMenu);
+  }
+
+
+return (
+    <div className='navbar flex items-center justify-between p-4 z-[100] w-full absolute '>
+    <Link to='/'>
+      <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>
+        MOVIES-DB
+      </h1>
+    </Link>
+    
+    
+    
+      <div onClick={handleToggle} className='navbar__menu relative z-999 '>
+
+        {
+          toggleMenu ? (
+            <RiCloseLine 
+            size={26}  
+            color={'red'} 
+             />
+
+          ) : (
+
+            <RiMenu3Line 
+            size={26}  
+            color={'red'} 
+            />
+          )
+        }
+
+       <ul className={toggleMenu ? 
+        'fixed right-0 top-0 w-[20%] h-full border-r border-r-gray-900 bg-[#000300]/80 ease-in-out duration-500' 
+        : 'ease-in-out duration-500 fixed right-[-100%]'}>
+          <li className='text-red-600 font-bold  p-4 border-b border-gray-600'>Home</li>
+          <li className=' text-red-600 font-bold  p-4 border-b border-gray-600'>Company</li>
+          <li className=' text-red-600 font-bold  p-4 border-b border-gray-600'>Resources</li>
+          <li className=' text-red-600 font-bold  p-4 border-b border-gray-600'>About</li>
+          <li className='text-red-600 font-bold  p-4'>Contact</li>
+       
+       </ul>
+
+
+        
+      
+
             
-            <h1 className='text-red-600 font-bold text-4xl'>MOVIES-DB</h1>
-            <ImSearch className="ml-4"size={28} color={'red'}/>
-            
-            </div>
-
-
-
-    </div>
+        
+      </div>
+    
+  </div>
   )
 }
 
 export default Navbar
+
+
+{/* <input
+                onChange={(e) => setSearch(e.target.value)}
+                className='p-3 my-2 bg-gray-700 rouded'
+                type='search'
+                placeholder='Search'
+                autoComplete='search'
+              />
+
+              {console.log(search)} */}
